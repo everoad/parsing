9abecce1c93892a1cbcc1f219b8163f1c75cc91b
DESKTOP-SCQ529V\kbj <everoad102@gmail.com>
2017-06-18 14:57:49 +0900

    POI 버전 수정 및 xlsx, xls 구분 없이 사용가능. gradle bootRepackage

--- src/main/java/parsing/ParsingApplication.java
+++ src/main/java/parsing/ParsingApplication.java
@@ -31,7 +31,7 @@ public class ParsingApplication {
 
 		MainView view = ctx.getBean(MainView.class);
 		view.setVisible(true);
-
+		
 	}
 
 	@Bean
--- src/main/java/parsing/service/ParsingService.java
+++ src/main/java/parsing/service/ParsingService.java
@@ -1,7 +1,17 @@
 package parsing.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import parsing.mapper.ParsingMapper;
@@ -9,8 +19,11 @@ import parsing.vo.BoardVO;
 import parsing.vo.LteVO;
 
 
+
 public class ParsingService {
 
+	
+	
 	@Autowired
 	private ParsingMapper parsingMapper;
 	
@@ -24,6 +37,54 @@ public class ParsingService {
 		}
 		
 		return null;
+	}
+	
+	
+	
+	
+	//수정사항.
+	public void test() throws IOException, EncryptedDocumentException, InvalidFormatException {
+
+		FileInputStream fis = new FileInputStream(new File("C:/java/test.xlsx"));
+
+		Workbook wb = WorkbookFactory.create(fis);
+
+		Sheet sheet = wb.getSheetAt(0);
+
+		if (sheet != null) {
+			
+			int rows = sheet.getPhysicalNumberOfRows();
+			
+			for (int r = 0; r < rows; r++) {
+				
+				Row row = sheet.getRow(r);
+				
+				if (row != null) {
+					
+					int cells = row.getPhysicalNumberOfCells();
+					
+					for (int c = 0; c < cells; c++) {
+						
+						Cell cell = row.getCell(c);
+
+						switch (cell.getCellTypeEnum()) {
+						
+						case STRING:
+							System.out.println("String : " + cell.getStringCellValue());
+							break;
+							
+						case NUMERIC:
+							System.out.println("Integer : " + cell.getNumericCellValue());
+							break;
+							
+						default:
+							break;
+						}
+					}
+				}
+			}
+		}
+
 	}
 	
 	
